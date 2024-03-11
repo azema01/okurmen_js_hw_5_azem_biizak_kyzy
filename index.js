82d1f212a465dae1573d2989d81878ae8e6ff15a
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-11 10:39:42 +0600

    js 5

---
+++ index.js
@@ -0,0 +1,73 @@
+// const fruits = [];
+// const vegetables =['AK-ORGO','Turusbekova kievskii', 'Maximum'];
+
+// fruits.push('AK-ORGO');
+// fruits.push('Turusbekova kievski');
+// fruits.push('Maximum');
+// console.log(fruits);
+
+// const newArrayProducts = vegetables.concat(fruits);
+// const arrayConvertString = newArrayProducts.join(',\n ');
+
+
+ 
+
+
+ const fruits = [];
+ const arrayOfBooks =['Будь лучшей версией себя','Подсознание может все', 'Искусство жить просто', 'Магическая уборка', 'Меньше значит Болше'];
+ 
+fruits.push('-Меньше значит Болше');
+fruits.push('-Магическая уборка');
+fruits.push('-Искусство жить просто');
+fruits.push('-Подсознание может все');
+fruits.push('-Будь лучшей версией себя');
+// console.log(fruits);
+
+// const newArrayProducts = arrayOfBooks.join(fruits);
+const newArrayProducts = arrayOfBooks.concat(fruits);
+const string = fruits.join('\n');
+console.log(string);
+
+
+
+// const fruits = [];
+
+// fruits.push('AMIR');
+// fruits.push('ADAM');
+// fruits.push('EMIR');
+// fruits.push('BEKBOL');
+// fruits.push('ADIL');
+
+// const fruits1 = [];
+
+// fruits.push('ADEMI');
+// fruits.push('AGATA');
+// fruits.push('MAYA');
+// fruits.push('AZALYA');
+// fruits.push('MARIA');
+
+// console.log(fruits);
+
+// const okuuchular = fruits.concat(fruits1);
+
+
+
+
+
+// let flowers = ['ROZA', 'TULPAN', 'PEONY'];
+
+// for (let i=0; i < flowers.length; i++){
+//     console.log( i + 1 + ' ' + flowers[i]);
+// }
+
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// for (let i = 0;  i < numbers.length; i++){
+//     numbers[i] *= 5;   
+// }
+
+// console.log(numbers);
